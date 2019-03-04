89e42ab48bc305f375a35d6a57fa2601e3b88963
Leonardo Lama <leolama18@gmail.com>
2019-03-04 18:31:01 -0400

    Tu ere bella como la banana

--- webscrapping-main/scrapper.py
+++ webscrapping-main/scrapper.py
@@ -1,7 +1,8 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import csv
+import xlwt
+from xlwt import Workbook
 import pandas as pd
 
 class PythonOrgSearch(unittest.TestCase):
@@ -15,8 +16,11 @@ class PythonOrgSearch(unittest.TestCase):
         contador3 = 1
         datos_for_csv = []
         huachicolero = 1
+        savior = 0
+        wb = Workbook()
+        sheet1 = wb.add_sheet('Sheet 1')
         """ Aqui recorremos cada pagina de este """
-        for k in range(100):
+        for k in range(101):
             lista_de_clientes = []
             wakanda = []
             contador1 = 2
@@ -27,35 +31,39 @@ class PythonOrgSearch(unittest.TestCase):
                 premail = driver.find_element_by_id("ContentPlaceHolder1_gvAngentesInmobiliarios_imbdetalle_" + str(contador2))
                 premail.click()
                 correo = driver.find_element_by_id("ContentPlaceHolder1_lblCorreo").text
-                lista_de_clientes = [['Nombre'+str(x), name], ['Correo'+str(x), correo]]
-                wakanda.append(lista_de_clientes)
+                sheet1.write(savior, 0, name)
+                sheet1.write(savior, 1, correo)
+                wb.save('list_clientes.xls')
                 contador1+=1
                 contador2+=1
+                savior+=1
+                print('esta pagina va por el elemento:'+ str(x))
             """ Aqui se guarda la lista con los 10 clientes de la primera pagina """
             if contador3 == 10: 
                 huachicolero = 1
+                savior+=1
+                print('ya contador3 vale 10 y paso bien por aca')
                 killmonger = driver.find_element_by_xpath("/html/body/form/div[4]/div[3]/div[1]/table/tbody/tr[12]/td/table/tbody/tr/td[11]")
-            elif contador3 == 20 or contador3 == 30 or contador3 == 40 or contador3 == 50 or contador3 == 60 or contador3 == 70 or contador3 == 80 or contador3 == 90 or contador3 == 100:
+            elif contador3 >= 20 and (contador3%10) == 0:
                 huachicolero = 1
+                print('ya contador3 vale '+str(contador3)+' y sigue bien')
+                savior+=1
                 killmonger = driver.find_element_by_xpath("/html/body/form/div[4]/div[3]/div[1]/table/tbody/tr[12]/td/table/tbody/tr/td[12]")
+            elif contador3 == 51:
+                killmonger = driver.find_element_by_xpath("/html/body/form/div[4]/div[3]/div[1]/table/tbody/tr[12]/td/table/tbody/tr/td[4]")
+                print('me salte el 51 jiji, voy por el: '+str(contador3))
             else:
                 killmonger = driver.find_element_by_xpath("/html/body/form/div[4]/div[3]/div[1]/table/tbody/tr[12]/td/table/tbody/tr/td["+str(huachicolero+1)+"]")
+                savior+=1
+                print('la secuencia ya vale: '+str(contador3))
                 huachicolero+=1
-            killmonger.click()
-            datos_for_csv.append(wakanda)   
+            killmonger.click() 
             contador3+=1                    
-        """ Area donde se imprime en el CSV (y en la consola) """
-        print(datos_for_csv)
-        
-        with open("prueba.csv", "w") as csv_file:
-                writer = csv.writer(csv_file, delimiter=',')
-                for line in datos_for_csv:
-                    writer.writerow(line)
-        print('----------frontera-colombo-venezolana----------')
-
+        """ Area donde se imprime en el CSV (y en la consola) """        
 
     def tearDown(self):
-        self.driver.close()
+        pass
+        # self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
--- webscrapping-main/script.py
+++ webscrapping-main/script.py
@@ -1,28 +1,13 @@
-import unittest
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-import requests
-import csv
+import xlwt
+from xlwt import Workbook
 
-class PythonOrgSearch(unittest.TestCase):
+dic = ['John, john@example.com', 'Mary, mary@example.com', 'John, john@example.com', 'Mary, mary@example.com', 'John, john@example.com', 'Mary, mary@example.com', 'John, john@example.com', 'Mary, mary@example.com'] #dictionary
+wb = Workbook()
+n = 0
+k = 0
+sheet1 = wb.add_sheet('Sheet 1') 
+for data in dic:
+    sheet1.write(n, 0, data)
+    n+1
 
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-
-    def test_search_inmoperu(self):
-        driver = self.driver
-        driver.get("http://agenteinmobiliario.vivienda.gob.pe/AgentesInmobiliarios.aspx")
-        elem = driver.find_element_by_xpath("/html/body/form/div[4]/div[3]/div[1]/table/tbody/tr[2]/td[2]")
-        elem1 = driver.find_element_by_id("ContentPlaceHolder1_gvAngentesInmobiliarios_imbdetalle_0")
-        elem1.click()
-        correo = driver.find_element_by_id("ContentPlaceHolder1_lblCorreo")
-        print(correo.text)
-        
-        
-
-    def tearDown(self):
-        self.driver.close()
-
-if __name__ == "__main__":
-    unittest.main()
+wb.save('clientess.xls')
